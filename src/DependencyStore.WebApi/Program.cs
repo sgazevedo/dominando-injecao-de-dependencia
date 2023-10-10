4c78464c8356a389473571eeb7cb916c24612d98
sgazevedo <sgazevedo@gmail.com>
2023-10-10 15:40:21 -0300

    feat(): adicionado injecao de dependencias de conexao com banco de dados, repositorios e services

--- src/DependencyStore.WebApi/Program.cs
+++ src/DependencyStore.WebApi/Program.cs
@@ -1,6 +1,17 @@
+using DependencyStore.WebApi.Repositories;
+using DependencyStore.WebApi.Repositories.Contracts;
+using DependencyStore.WebApi.Services;
+using DependencyStore.WebApi.Services.Contracts;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped(x => new SqlConnection("CONN_STRING"));
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
