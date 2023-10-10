07e8b07507ddcc1c6ac2bcf6613facd420669c56
sgazevedo <sgazevedo@gmail.com>
2023-10-10 15:28:03 -0300

    feat(): movido rtegra de calculo de pedido para classe order

--- src/DependencyStore.WebApi/Models/Order.cs
+++ src/DependencyStore.WebApi/Models/Order.cs
@@ -2,12 +2,21 @@ namespace DependencyStore.WebApi.Models
 {
   public class Order
   {
+    public Order(decimal deliveryFee, decimal discount, List<Product> products)
+    {
+      Code = Guid.NewGuid().ToString().ToUpper()[..8];
+      Date = DateTime.Now;
+      DeliveryFee = deliveryFee;
+      Discount = discount;
+      Products = products;
+    }
+
     public string Code { get; set; }
     public DateTime Date { get; set; }
     public decimal DeliveryFee { get; set; }
     public decimal Discount { get; set; }
-    public int[] Products { get; set; }
-    public decimal SubTotal { get; set; }
-    public decimal Total { get; set; }
+    public List<Product> Products { get; set; }
+    public decimal SubTotal => Products.Sum(x => x.Price);
+    public decimal Total => SubTotal - Discount + DeliveryFee;
   }
 }
